cb69b17ebc7ad2e58a0fc228d5215a165822290c
twistedwisdom <twistedwisdom@gmail.com>
2013-05-06 17:53:28 -0500

    Renamed and Added config file to eCP

--- DealerPortal/Account.cs
+++ DealerPortal/Account.cs
@@ -8,7 +8,7 @@ using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 using System.IO;
 
-namespace DealerPortalTests
+namespace DealerPortal
 {
     [TestFixture]
     public class Accounts
