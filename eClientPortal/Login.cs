fe64855ab11fa49ec054bac84c0f843fd6b2a3a5
twistedwisdom <twistedwisdom@gmail.com>
2013-05-07 13:16:03 -0500

    eCP Test - Login Test success

---
+++ eClientPortal/Login.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System.IO;
+
+namespace eClientPortal
+{
+    [TestFixture]
+    public class Login
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private bool acceptNextAlert = true;
+        private string testResult;
+        private string testResultReason;
+        private static string baseURL = Config.startURL;
+        private static string testUserName = Config.testUserName;
+        private static string testPassWord = Config.testPassWord;
+        private static string auxUserName = Config.auxUserName;
+
+        [SetUp]
+        public void SetupTest()
+        {
+            driver = new FirefoxDriver();
+            verificationErrors = new StringBuilder();
+            testResult = null;
+            testResultReason = null;
+            Config.ReadConfig("config.xml");
+        }
+
+        [Test]
+        public void LoginLogOut()
+        {
+            driver.Navigate().GoToUrl(Config.startURL + "Login.aspx");
+            driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ctrlLogin_Login1_UserName")).Clear();
+            driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ctrlLogin_Login1_UserName")).SendKeys(Config.testUserName);
+            driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ctrlLogin_Login1_Password")).Clear();
+            driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ctrlLogin_Login1_Password")).SendKeys(Config.testPassWord);
+            driver.FindElement(By.Id("ctl00_ContentPlaceHolder1_ctrlLogin_Login1_BtnSubmit")).Click();
+            // Warning: verifyTextPresent may require manual changes
+            try
+            {
+                Assert.IsTrue(Regex.IsMatch(driver.FindElement(By.CssSelector("BODY")).Text,
+                                            "^[\\s\\S]* MY ACCOUNT OVERVIEW[\\s\\S]*$"));
+            }
+            catch (AssertionException e)
+            {
+                verificationErrors.Append(e.Message);
+            }
+            driver.FindElement(By.Id("ctl00_ctlRightNavbar_btnLogout")).Click();
+
+
+        }
+    }
+}
---
+++ eClientPortal/_config.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using System.Web;
+namespace eClientPortal
+{
+
+    public static class Config
+    {
+     
+        public static string startURL;
+        public static string testUserName;              // Username of selenium test user. 
+        public static string testPassWord;             // Password for selenium test user. Must have admin role. 
+        public static string auxUserName;             // An account for tinkering with
+        public static string auxEmailAddr;           // Email address associated with auxUserName
+
+        public static void ReadConfig(string fileName)
+        {
+            try
+            {
+                XDocument doc = XDocument.Load(fileName);
+                
+
+                foreach (XElement el in doc.Root.Elements())
+                {
+                    foreach (XElement element in el.Elements())
+                    {
+                        string eledata = element.Value;
+                        string elename = element.Name.ToString();
+
+                        if (elename == "baseURL")
+                        {
+                            startURL = eledata;
+                        }
+                        if (elename == "testUserName")
+                        {
+                            testUserName = eledata;
+                        }
+                        if (elename == "testPassWord")
+                        {
+                            testPassWord = eledata;
+                        }
+                        if (elename == "auxUserName")
+                        {
+                            auxUserName = eledata;
+                        }
+                        if (elename == "auxEmailAddr")
+                        {
+                            auxEmailAddr = eledata;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+        }
+
+    }
+}
+
